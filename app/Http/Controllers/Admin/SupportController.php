2056ca4683ca4e8c1ef0190501605b4a34c2d19f
JOSE ALOISIO ARAGAO MAGALHAES <aloisio.aragao@uvanet.br>
2024-07-01 16:26:42 -0300

    Aulas 07 - 09

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\support;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    public function index(Support $support)
+    {
+        $supports = $support->all();
+        dd($supports);
+        return view('admin/supports/index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/suporte', [SupportController::class, 'index'])->name('suporte.index');
 
 Route::get('/contato', [SiteController::class, 'contact']);
 
