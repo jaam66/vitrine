13f234c3e28d71b6e54a736d3fa87d36d10bae76
Aloisio <jaam.ce@gmail.com>
2024-08-12 10:22:47 -0300

    Correção CSS suporte

--- resources/views/admin/supports/partials/form.blade.php
+++ resources/views/admin/supports/partials/form.blade.php
@@ -13,7 +13,7 @@
     <label for="assunto">Texto*:</label>
     <textarea name="body" cols="30" rows="5" placeholder="Descrição" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">{{ $support->body ?? old('body') }}</textarea>
     <div class="text-center w-full">
-        <button type="submit" class="enviar_form">
+        <button type="submit" class="enviarForm">
             Enviar
         </button>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,16 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/dashboard', function () {
+    return view('auth/login');
+});
 Route::get('/', function () {
     return view('auth/login');
 });
 // -----------------------------------------------------------------------------------------
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 
 Route::middleware('auth')->group(function () {
